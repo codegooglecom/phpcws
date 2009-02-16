48401c9bc538d5aa4b0464f390440fa2059cdf80
blog.s135.com <blog.s135.com@51fe7d86-dbaf-11dd-9f21-5bfbb98aebe5>
2009-02-16 03:07:21 +0000

    PHPCWS 演示程序

---
+++ php-example/example.php
@@ -0,0 +1,17 @@
+<?php
+@header('Content-Type: text/html; charset=gb2312');
+$text = "2009年2月13日，我编写了一款PHP中文分词扩展：PHPCWS 1.0.0。";
+
+echo "分词结果为：<BR>";
+echo phpcws_split($text);
+
+echo "<BR><BR>";
+
+echo "过滤掉默认标点符号的分词结果为：<BR>";
+echo phpcws_split($text, "default");
+
+echo "<BR><BR>";
+
+echo "过滤掉全角逗号与冒号的分词结果为：<BR>";
+echo phpcws_split($text, "，：");
+?>
