2b64715fcfdbd9f434d4721424bcdff6f48c0c07
blog.s135.com <blog.s135.com@51fe7d86-dbaf-11dd-9f21-5bfbb98aebe5>
2009-02-16 06:00:16 +0000

    phpcws_split函数调用示例

--- trunk/php-example/example_gbk.php
+++ trunk/php-example/example_gbk.php
@@ -3,15 +3,14 @@
 $text = "2009年2月13日，我编写了一款PHP中文分词扩展：PHPCWS 1.0.0。";
 
 echo "分词结果为：<BR>";
-echo phpcws_split($text);
-
-echo "<BR><BR>";
+$result = phpcws_split($text);
+echo $result . "<BR><BR>";
 
 echo "过滤掉默认标点符号的分词结果为：<BR>";
-echo phpcws_split($text, "default");
-
-echo "<BR><BR>";
+$result = phpcws_split($text, "default");
+echo $result . "<BR><BR>";
 
 echo "过滤掉全角逗号与冒号的分词结果为：<BR>";
-echo phpcws_split($text, "，：");
+$result = phpcws_split($text, "，：");
+echo $result . "<BR><BR>";
 ?>
